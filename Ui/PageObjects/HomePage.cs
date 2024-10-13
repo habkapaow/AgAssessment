e6d31230c0ca71ba3daab5ad129b61d96a8a1e7d
Ehab Jallad <ejallad@cpaxtra.co.th>
2024-10-13 23:27:55 +0700

    Added UI project and tests Added ReadMe Fixed Solution structure

---
+++ Ui/PageObjects/HomePage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+
+namespace UIAutomation.PageObjects
+{
+    public class HomePage
+    {
+        private readonly IWebDriver driver;
+        private readonly By solutionsMenu = By.LinkText("SOLUTIONS");
+        private readonly By marketIntelligenceSubMenu = By.LinkText("Market Intelligence");
+
+        public HomePage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+
+        /// <summary>
+        /// Clicks the solution menu on top navigation
+        /// </summary>
+        public void ClickSolutionsMenu()
+        {
+            driver.FindElement(solutionsMenu).Click();
+        }
+
+        /// <summary>
+        /// Cliks on Market Intelligence sub-menu from Soluions menu
+        /// </summary>
+        /// <returns>Market Intelligence Page</returns>
+        public MarketIntelligencePage ClickMarketIntelligenceSubMenu()
+        {
+            driver.FindElement(marketIntelligenceSubMenu).Click();
+            return new MarketIntelligencePage(driver);
+        }
+    }
+}
